07cffe5d01c66a7ccae13293eb88c2dc8fb20548
Alex <tosi.alex13@gmail.com>
2019-10-01 15:40:22 +0200

    Ajout fonctions

---
+++ graphUtils.c
@@ -0,0 +1,37 @@
+#include "graphUtils.h"
+
+int arc(graphe_l g, sommet x, sommet y)
+{
+  liste *tmp = g.a[x];
+
+  if(g.n <= x || g.n <= y)
+    return 0;
+
+  while(tmp != NULL) {
+    if(g.a[x]->suivant.st == y)
+      return 1;
+    tmp = tmp->suivant;
+  }
+
+  return 0;
+}
+
+void successeurs(graphe_l g, sommet x, ens_de_sommets *e)
+{
+  liste *l;
+  l = g.a[x];
+  e = NULL;
+
+  while(l != NULL) {
+    addArc(e, l->st);
+    l = l->suivant;
+  }
+}
+
+void addArc(ens_de_sommets *e, sommet y)
+{
+  ens_de_sommets *p = malloc(sizeof(Couple));
+  p->st = y;
+  p->suivant = e;
+  e = p;
+}
