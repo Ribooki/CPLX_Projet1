b466bf1cfa1de5c3d0369884a66a4148f7724626
Alex <tosi.alex13@mail.com>
2019-10-01 15:58:25 +0200

    Réglage des erreurs

--- graphUtils.c
+++ graphUtils.c
@@ -2,13 +2,13 @@
 
 int arc(graphe_l g, sommet x, sommet y)
 {
-  liste *tmp = g.a[x];
+  liste tmp = g.a[x];
 
   if(g.n <= x || g.n <= y)
     return 0;
 
   while(tmp != NULL) {
-    if(g.a[x]->suivant.st == y)
+    if(g.a[x]->suivant->st == y)
       return 1;
     tmp = tmp->suivant;
   }
@@ -18,20 +18,20 @@ int arc(graphe_l g, sommet x, sommet y)
 
 void successeurs(graphe_l g, sommet x, ens_de_sommets *e)
 {
-  liste *l;
+  liste l;
   l = g.a[x];
   e = NULL;
 
   while(l != NULL) {
-    addArc(e, l->st);
+    addVertex(e, l->st);
     l = l->suivant;
   }
 }
 
-void addArc(ens_de_sommets *e, sommet y)
+void addVertex(ens_de_sommets *e, sommet y)
 {
-  ens_de_sommets *p = malloc(sizeof(Couple));
+  ens_de_sommets p = malloc(sizeof(Couple));
   p->st = y;
-  p->suivant = e;
-  e = p;
+  p->suivant = *e;
+  e = &p;
 }
--- graphUtils.h
+++ graphUtils.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #define n_max 1000
 
 typedef int sommet;
@@ -8,8 +11,18 @@ typedef struct chainon {
 } Couple;
 
 typedef Couple *liste;
+typedef liste ens_de_sommets;
 
 typedef struct {
 	liste a[n_max];
 	int n;
 } graphe_l;
+
+/* Renvoie 1 s'il existe un arc entre le sommet x et y, et 0 sinon */
+int arc(graphe_l g, sommet x, sommet y);
+
+/* Ajoute les successeurs du sommet x dans ens_de_sommets e */
+void successeurs(graphe_l g, sommet x, ens_de_sommets *e);
+
+/* Ajoute le sommet y dans l'ens_de_sommets e */
+void addVertex(ens_de_sommets *e, sommet y);
