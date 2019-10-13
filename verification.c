2b1b2436d13d66e83d66c507fc75c32d2e8a74ee
Letose <tosi.alex13@gmail.com>
2019-10-13 14:42:41 +0200

    Ajout du pseudo-code du rapport + indications pour isMaximal()

--- verification.c
+++ verification.c
@@ -15,3 +15,20 @@ int isDesert(graphe_l g, ens_de_sommets *e)
 
   return 1;
 }
+
+
+int isMaximal(graphe_l g, ens_de_sommets *e)
+{
+  if(!isDesert(g, e))
+    return 0;
+
+  /*
+  tant qu'on arrive pas à la fin alors
+    parcours tous les sommets
+      on ajoute un sommet qui n'est pas déjà dans e et on vérifie que le graphe est tjr désert
+      si non alors
+        on retire le sommet
+      sinon
+        c'en est un et wlh c pa macksymmâlleux
+  */
+}
