edf3a5826060e82866ea7f53af8b175f8fcd3489
Letose <tosi.alex13@gmail.com>
2019-11-12 15:15:25 +0100

    petit chingeming

--- verification.c
+++ verification.c
@@ -18,15 +18,16 @@ int isDesert(graphe_l g, ens_de_sommets *e)
 
 int isMaximal(graphe_l g, ens_de_sommets *e)
 {
-  if(!isDesert(g, e))
+    graphe_l tmp = g;
+  if(!isDesert(tmp, e))
       return 0;
 
-  for(int i = 0 ; i < g.n ; i++) {
+  for(int i = 0 ; i < tmp.n ; i++) {
       /* Si i n'est pas déjà un sommet, on l'ajoute puis on teste si le graphe
       ** est désert */
       if(!contains(e, i)) {
           addVertex(e, i);
-          if(!isDesert(g, e))   //si pas désert on continue
+          if(!isDesert(tmp, e))   //si pas désert on continue
             removeFirst(e);
           else                  //sinon e n'est pas maximal
             return 0;
