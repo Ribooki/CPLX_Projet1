bd2c3f8a6ca6e15f8d40ecc82b176458dc8334a9
megastrikerhd@outlook.com <megastrikerhd@outlook.com>
2019-11-12 15:37:35 +0100

    comme nanavnt nha

--- maximal.h
+++ maximal.h
@@ -1,4 +1,2 @@
 #include "utils.h"
 
-/* Retourne 1 si le sous-graphe est maximal, et 0 sinon */
-int isMaximal(graphe_l g, ens_de_sommets *e);
--- verification.h
+++ verification.h
@@ -2,3 +2,6 @@
 
 /* Retourne 1 si le sous-graphe e est désert, et 0 sinon */
 int isDesert(graphe_l g, ens_de_sommets *e);
+
+/* Retourne 1 si le sous-graphe est maximal, et 0 sinon */
+int isMaximal(graphe_l g, ens_de_sommets *e);
