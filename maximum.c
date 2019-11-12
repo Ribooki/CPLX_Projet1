215003d19b2cfa3ca7112b635b1ffbddbdec1259
megastrikerhd@outlook.com <megastrikerhd@outlook.com>
2019-11-12 18:30:57 +0100

    maximum changes

--- maximum.c
+++ maximum.c
@@ -1,43 +1,36 @@
 #include "maximum.h";
 
-void calcul_maximum_exact(graphe_l g, int k){
-    int *sommetsChoisis = malloc(sizeof(int)*g.e);
-    int *sommetsSelectionnes = malloc(sizeof(int)*g.e);
-    for(int i=0; i<g.e; i++){
-        sommetsChoisis[i] = 0;
-        sommetsSelectionnes[i] = 0;
+int calcul_maximum_exact(graphe_l g, int * sommetsRetires, int *sommetsSelectionables,int k){
+    if()
+    if(sommetsRetires == NULL){
+        sommetsRetires = malloc(sizeof(int)*g.e);
+        for(int i=0; i<g.e; i++){
+            sommetsRetires[i] = 0;
+        }
     }
-    /**
-        free(listeDesVoisins);
-        memcpy(listeDesVoisins, g.a[i], sizeof(g.a[i]));
-        
-    **/
+    if(sommetsSelectionables == NULL){
+        sommetsSelectionables = malloc(sizeof(int)*g.e);
+        for(int i=0; i<g.e; i++){
+            sommetsSelectionables[i] = 1;
+        }
+    }
+
+    //vériferi quand sommet est delectionnable ou non
+
     liste listeDesVoisins;
     for(int i=0; i<g.e; i++){
-        sommetsSelectionnes[i] = 1;
+        sommetsRetires[i] = 1;
+        sommetsSelectionables[i] = 0;
         free(listeDesVoisins);
         memcpy(listeDesVoisins, g.a[i], sizeof(g.a[i]));
         while(listeDesVoisins != NULL){
-            sommetsSelectionnes[listeDesVoisins->st] = 1;
+            if(sommetsSelectionables[i] == 1){
+                sommetsRetires[listeDesVoisins->st] = 1;
+            }
             listeDesVoisins = listeDesVoisins->suivant;
         }
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        sommetsChoisis[i] = 0;
+        if(calcul_maximum_exact(g, sommetsRetires, sommetsSelectionables,k-1)){
+            return 1;
+        }
     }
 }
