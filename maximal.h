bb8eb37550e0c1ff515888adfce24c46d18f115d
megastrikerhd@outlook.com <megastrikerhd@outlook.com>
2019-11-12 15:34:55 +0100

    change place of maximal funct

---
+++ maximal.h
@@ -0,0 +1,4 @@
+#include "utils.h"
+
+/* Retourne 1 si le sous-graphe est maximal, et 0 sinon */
+int isMaximal(graphe_l g, ens_de_sommets *e);
--- verification.c
+++ verification.c
@@ -15,23 +15,3 @@ int isDesert(graphe_l g, ens_de_sommets *e)
 
   return 1;
 }
-
-int isMaximal(graphe_l g, ens_de_sommets *e)
-{
-    graphe_l tmp = g;
-  if(!isDesert(tmp, e))
-      return 0;
-
-  for(int i = 0 ; i < tmp.n ; i++) {
-      /* Si i n'est pas déjà un sommet, on l'ajoute puis on teste si le graphe
-      ** est désert */
-      if(!contains(e, i)) {
-          addVertex(e, i);
-          if(!isDesert(tmp, e))   //si pas désert on continue
-            removeFirst(e);
-          else                  //sinon e n'est pas maximal
-            return 0;
-      }
-  }
-  return 1;
-}
