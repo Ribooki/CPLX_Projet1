45bc6117909e6b6a98f499a7e0ce07c6fc6f99ba
Dr-LeBro <megastrikerhd@outlook.com>
2020-01-03 14:05:11 +0100

    sat data

--- satUtils.h
+++ satUtils.h
@@ -1,2 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef int sommet;
+
+typedef struct {
+    struct clause *clauses;
+    int clausesCount;
+} sat;
+
+typedef struct {
+    struct vertex *vertices;
+} clause;
+
+typedef struct{
+    int sommet *sommets;
+    int isNegative;
+} vertex;
